e659385fca31213006fb27708f52d02661b66071
Javier Peces <javierpeces@users.noreply.github.com>
2016-10-05 17:43:20 +0200

    Create 00-introduction.sh

---
+++ 00-introduction.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#
+# This Step by Step Shell Script Language course is dedicated to Elena.
+# You won't learn a lot here, but the basics to start working are present and samples are self explaining.
+#
+# Do not hesitate to comment or contact if needed.
+#
+# ENJOY!!!
+
+echo "hello, world!"
